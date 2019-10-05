87cbd23427c4d39c565f4d47125f66df0ced07a0
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-05 15:09:59 -0500

    Add new gpf to replace urldecode logic.

---
+++ gpf-07/app.js
@@ -0,0 +1,33 @@
+const aws = require('aws-sdk');
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * @param {Object} context
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.handler = async (event) => {
+    var stepfunctions = new aws.StepFunctions();
+    let body = event['body'].split('+').join(' ');
+    let payload = decodeURIComponent(body).split('payload=')[1];
+    console.log(payload);
+    var params = {
+      stateMachineArn: 'arn:aws:states:us-east-2:990217436416:stateMachine:emailReport',
+      input: payload
+    };
+    let response = await new Promise((resolve, reject) => {
+      stepfunctions.startExecution(params, (err, data) => {
+        if (err) reject(err, err.stack); // an error occurred
+        else     resolve(data);           // successful response
+      });
+    });
+    return {
+      statusCode: 200
+    };
+};
