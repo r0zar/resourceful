32dc4b574f32037f1f964bca3be4d5f6082eb77d
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-19 20:58:07 -0500

    Add vote logi

--- gpf-02/app.js
+++ gpf-02/app.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,6 +14,65 @@
  */
 exports.lambdaHandler = async (event, context) => {
     
-    return await Promise.resolve(event);
+    let blocks = [
+  		{
+  			"type": "section",
+  			"text": {
+  				"type": "mrkdwn",
+  				"text": "Should we test if users would like the option of a 'dark mode' in farmshots?"
+  			}
+  		},
+  		{
+  			"type": "actions",
+  			"elements": [
+  				{
+  					"type": "button",
+  					"text": {
+  						"type": "plain_text",
+  						"emoji": true,
+  						"text": "Downvote"
+  					},
+  					"style": "danger",
+  					"value": "click_me_123"
+  				},
+  				{
+  					"type": "button",
+  					"text": {
+  						"type": "plain_text",
+  						"emoji": true,
+  						"text": "Skip"
+  					},
+  					"value": "click_me_123"
+  				},
+  				{
+  					"type": "button",
+  					"text": {
+  						"type": "plain_text",
+  						"emoji": true,
+  						"text": "Cancel"
+  					},
+  					"value": "click_me_123"
+  				},
+  				{
+  					"type": "button",
+  					"text": {
+  						"type": "plain_text",
+  						"emoji": true,
+  						"text": "Upvote"
+  					},
+  					"style": "primary",
+  					"value": "click_me_123"
+  				}
+  			]
+  		}
+  	]
+    
+    let body = {
+        "response_type": "in_channel",
+        "text": 'User test created!',
+        "blocks": blocks
+      };
+    
+    return await axios.post(event.response_url, body).then(response => response.status);
     
 };
