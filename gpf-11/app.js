b3a4c059030361234bba70e7545e38e0eb9ee91b
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-26 02:23:19 -0500

    Update with modal.

--- gpf-11/app.js
+++ gpf-11/app.js
@@ -14,6 +14,38 @@ const axios = require('axios');
  */
 exports.lambdaHandler = async (event, context) => {
 
-	return await Promise.resolve(event);
+
+	const responseBody = {
+		"trigger_id": event.trigger_id,
+		"view": {
+			"type": "modal",
+			"callback_id": "modal-identifier",
+			"title": {
+				"type": "plain_text",
+				"text": "Just a modal"
+			},
+			"blocks": [
+				{
+					"type": "section",
+					"block_id": "section-identifier",
+					"text": {
+						"type": "mrkdwn",
+						"text": "*Welcome* to ~my~ Block Kit _modal_!"
+					},
+					"accessory": {
+						"type": "button",
+						"text": {
+							"type": "plain_text",
+							"text": "Just a button",
+						},
+						"action_id": "button-identifier",
+					}
+				}
+			],
+		}
+	};
+	
+	let response = await axios.post(event.response_url, responseBody);
+	return response.status;
 	
 };
