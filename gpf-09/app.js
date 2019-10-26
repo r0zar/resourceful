71ab53708a2fefafee87783294e5f5915f367b26
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-25 23:27:17 -0500

    Add axois and blocks reponse.

--- gpf-09/app.js
+++ gpf-09/app.js
@@ -32,7 +32,230 @@ exports.lambdaHandler = async (event) => {
         else     resolve(data);           // successful response
       });
     });
-    return {
-      statusCode: 200
+    const blocks = [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": ":microscope: Build a new self-improvement experiment!"
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*<fakeLink.toYourApp.com|Tension>*\nWhat's something that could be improved?"
+            },
+            "accessory": {
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Manage"
+                },
+                "options": [
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Edit it"
+                        },
+                        "value": "value-0"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Read it"
+                        },
+                        "value": "value-1"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Save it"
+                        },
+                        "value": "value-2"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*<fakeLink.toYourApp.com|Practice>*\nWhat do you propose we try?"
+            },
+            "accessory": {
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Manage"
+                },
+                "options": [
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Manage it"
+                        },
+                        "value": "value-0"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Read it"
+                        },
+                        "value": "value-1"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "Save it"
+                        },
+                        "value": "value-2"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*<fakeLink.toYourApp.com|Participants>*\nWho will be involved?"
+            },
+            "accessory": {
+                "action_id": "text1234",
+                "type": "multi_channels_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select channels"
+                }
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*<fakeLink.toYourApp.com|Duration>*\nWhen will you conduct a retrospective to collect perspectives and learning?"
+            },
+            "accessory": {
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Manage"
+                },
+                "options": [
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "1 Week"
+                        },
+                        "value": "value-0"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "2 Weeks"
+                        },
+                        "value": "value-1"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "3 Weeks"
+                        },
+                        "value": "value-2"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "4 Weeks"
+                        },
+                        "value": "value-3"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "*<fakeLink.toYourApp.com|Learning Metrics>*\nHow would you like to measure success?"
+            },
+            "accessory": {
+                "type": "static_select",
+                "placeholder": {
+                    "type": "plain_text",
+                    "emoji": true,
+                    "text": "Manage"
+                },
+                "options": [
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "% Completed"
+                        },
+                        "value": "value-0"
+                    },
+                    {
+                        "text": {
+                            "type": "plain_text",
+                            "emoji": true,
+                            "text": "KPI (success metric)"
+                        },
+                        "value": "value-1"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "divider"
+        },
+        {
+            "type": "actions",
+            "elements": [
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": "Launch"
+                    },
+                    "style": "primary",
+                    "value": "click_me_123"
+                },
+                {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "emoji": true,
+                        "text": "Cancel"
+                    },
+                    "value": "click_me_123"
+                }
+            ]
+        }
+    ]
+    const body = {
+      replace_original: true,
+      text: "Done ranking experiments.",
+      blocks: blocks
     };
+    
+    let response = await axios.post(event.response_url, body);
+    return response.status;
 };
