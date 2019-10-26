570ad3ce553a3bedbf6f74f59b46b6010eaa0370
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-25 23:47:29 -0500

    Slim down gpf9, add policies to sf lambdas.

--- gpf-09/app.js
+++ gpf-09/app.js
@@ -1,4 +1,3 @@
-const aws = require('aws-sdk');
 const axios = require('axios');
 
 /**
@@ -14,25 +13,15 @@ const axios = require('axios');
  * 
  */
 exports.lambdaHandler = async (event) => {
-    var stepfunctions = new aws.StepFunctions();
+
     let body = event['body'];
     let payload = {};
+    
     body.split('&').forEach(param => {
       let p = param.split('=');
       payload[p[0]] = decodeURIComponent(p[1]).split('+').join(' ');
     });
-    delete payload.token;
-    console.log(JSON.stringify(payload));
-    var params = {
-      stateMachineArn: 'arn:aws:states:us-east-2:990217436416:stateMachine:experiments-experiment',
-      input: JSON.stringify(payload)
-    };
-    await new Promise((resolve, reject) => {
-      stepfunctions.startExecution(params, (err, data) => {
-        if (err) reject(err, err.stack); // an error occurred
-        else     resolve(data);           // successful response
-      });
-    });
+
     const blocks = [
         {
             "type": "section",
