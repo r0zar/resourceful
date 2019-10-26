5606b57397d9c814be6e89f8c25abf7bebfd1949
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-26 00:50:44 -0500

    Add new lambda.

--- gpf-09/app.js
+++ gpf-09/app.js
@@ -1,4 +1,4 @@
-const axios = require('axios');
+const aws = require('aws-sdk');
 
 /**
  *
@@ -13,296 +13,26 @@ const axios = require('axios');
  * 
  */
 exports.lambdaHandler = async (event) => {
-
+    var stepfunctions = new aws.StepFunctions();
     let body = event['body'];
     let payload = {};
-    
     body.split('&').forEach(param => {
       let p = param.split('=');
       payload[p[0]] = decodeURIComponent(p[1]).split('+').join(' ');
     });
-
-    const blocks = [
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": ":microscope: Build a new self-improvement <https://theready.com/cards|experiment>!"
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*<fakeLink.toYourApp.com|Tension>*\nWhat's something that could be improved?"
-            },
-            "accessory": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Manage"
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Edit it"
-                        },
-                        "value": "value-0"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Read it"
-                        },
-                        "value": "value-1"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Save it"
-                        },
-                        "value": "value-2"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*<fakeLink.toYourApp.com|Practice>*\nWhat do you propose we try?"
-            },
-            "accessory": {
-                "type": "multi_static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Manage"
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Craft a clear and compelling purpose for the organization"
-                        },
-                        "value": "value-01"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Craft a clear and compelling purpose for every team and every role"
-                        },
-                        "value": "value-02"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Ask teams to share their essential intent for the next 6 to 24 months"
-                        },
-                        "value": "value-03"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Clarify the metrics that matter and use them to steer"
-                        },
-                        "value": "value-04"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Clarify the metrics that matter and use them to steer"
-                        },
-                        "value": "value-05"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Recognize and celebrate noble failure"
-                        },
-                        "value": "value-06"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Replace \"Is it perfect?\" with \"Is it safe to try?\""
-                        },
-                        "value": "value-07"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Give everyone the freedom to choose where, when and how they work"
-                        },
-                        "value": "value-08"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Clarify the decision rights held by teams and roles"
-                        },
-                        "value": "value-09"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "Use a _waterline_ to create guardrails around team and individual autonomy"
-                        },
-                        "value": "value-10"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*<fakeLink.toYourApp.com|Participants>*\nWho will be involved?"
-            },
-            "accessory": {
-                "action_id": "text1234",
-                "type": "multi_channels_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "text": "Select channels"
-                }
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*<fakeLink.toYourApp.com|Duration>*\nWhen will you conduct a retrospective to collect perspectives and learning?"
-            },
-            "accessory": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Manage"
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "1 Week"
-                        },
-                        "value": "value-0"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "2 Weeks"
-                        },
-                        "value": "value-1"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "3 Weeks"
-                        },
-                        "value": "value-2"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "4 Weeks"
-                        },
-                        "value": "value-3"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*<fakeLink.toYourApp.com|Learning Metrics>*\nHow would you like to measure success?"
-            },
-            "accessory": {
-                "type": "static_select",
-                "placeholder": {
-                    "type": "plain_text",
-                    "emoji": true,
-                    "text": "Manage"
-                },
-                "options": [
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "% Completed"
-                        },
-                        "value": "value-0"
-                    },
-                    {
-                        "text": {
-                            "type": "plain_text",
-                            "emoji": true,
-                            "text": "KPI (success metric)"
-                        },
-                        "value": "value-1"
-                    }
-                ]
-            }
-        },
-        {
-            "type": "divider"
-        },
-        {
-            "type": "actions",
-            "elements": [
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": "Launch"
-                    },
-                    "style": "primary",
-                    "value": "click_me_123"
-                },
-                {
-                    "type": "button",
-                    "text": {
-                        "type": "plain_text",
-                        "emoji": true,
-                        "text": "Cancel"
-                    },
-                    "value": "click_me_123"
-                }
-            ]
-        }
-    ]
-
-    const responseBody = {
-      replace_original: true,
-      text: "Create a new experiment!",
-      blocks: blocks
+    delete payload.token;
+    console.log(JSON.stringify(payload));
+    var params = {
+      stateMachineArn: 'arn:aws:states:us-east-2:990217436416:stateMachine:experiments-experiment',
+      input: JSON.stringify(payload)
     };
-    
-    let response = await axios.post(payload.response_url, responseBody);
-    return response.status;
+    await new Promise((resolve, reject) => {
+      stepfunctions.startExecution(params, (err, data) => {
+        if (err) reject(err, err.stack); // an error occurred
+        else     resolve(data);           // successful response
+      });
+    });
+    return {
+      statusCode: 200
+    };
 };
---
+++ gpf-10/tests/unit/test-handler.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const app = require('../../app.js');
+const chai = require('chai');
+const expect = chai.expect;
+var event, context;
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const result = await app.lambdaHandler(event, context)
+
+        expect(result).to.be.an('object');
+    });
+});
