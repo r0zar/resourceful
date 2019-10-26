963f7a59dda55d8a8440b929df3e39b5c4d33263
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-10-25 22:56:29 -0500

    Give step functions access to gpf 0.

--- gpf-09/tests/unit/test-handler.js
+++ gpf-09/tests/unit/test-handler.js
@@ -10,11 +10,5 @@ describe('Tests index', function () {
         const result = await app.lambdaHandler(event, context)
 
         expect(result).to.be.an('object');
-        expect(result.statusCode).to.equal(200);
-        expect(result.body).to.be.an('string');
-
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
     });
 });
