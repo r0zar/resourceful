c93a5ef46ba862a385f3f8ae85a9954a406f9f48
Ross Ragsdale <ross.ragsdale@gmail.com>
2019-11-02 15:30:52 -0500

    Revert.

--- gpf-01/app.js
+++ gpf-01/app.js
@@ -13,29 +13,5 @@ const axios = require('axios');
  * 
  */
 exports.lambdaHandler = async (event, context) => {
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-
-    console.log(JSON.stringify(event))
-    // // Parse the query params
-    // let mode = req.query['hub.mode'];
-    // let token = req.query['hub.verify_token'];
-    // let challenge = req.query['hub.challenge'];
-
-    // // Checks if a token and mode is in the query string of the request
-    // if (mode && token) {
-
-    //     // Checks the mode and token sent is correct
-    //     if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-            
-    //         // Responds with the challenge token from the request
-    //         console.log('WEBHOOK_VERIFIED');
-    //         res.status(200).send(challenge);
-
-    //     } else {
-    //         // Responds with '403 Forbidden' if verify tokens do not match
-    //         res.sendStatus(403);      
-    //     }
-    // }
     return await Promise.resolve(200)
 };
